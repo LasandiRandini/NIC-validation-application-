ec0ccfe11e3013834b188f86127fcbd7a4151c2f
LasandiRandini <randini-im20057@stu.kln.ac.lk>
2024-08-17 21:38:58 +0530

    change file upload

--- Backend/dashboard/src/index.js
+++ Backend/dashboard/src/index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
+
 app.use('/dashboard', dashboardRoutes);
 
-// Start the server
+
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Dashboard Service is running on port ${PORT}`);
